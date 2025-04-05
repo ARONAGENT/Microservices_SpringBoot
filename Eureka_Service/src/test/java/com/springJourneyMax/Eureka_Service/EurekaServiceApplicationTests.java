47a2fc9c45a8100be0cd1651c5cf45e82dca56da
Rohan Uke <ukerohan7@gmail.com>
2025-04-06 01:42:55 +0530

    add eureka service

---
+++ Eureka_Service/src/test/java/com/springJourneyMax/Eureka_Service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springJourneyMax.Eureka_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
